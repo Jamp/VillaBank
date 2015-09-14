2821763f4cb3c0f1c9629f451b7b60c22226fd7d
Jaro Marval <jampgold@gmail.com>
2015-09-14 17:21:39 -0430

    Inserción y separación de vistas

--- routes/jovenes.js
+++ routes/jovenes.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('jovenes', { title: 'Jóvenes', pretty: true });
+  res.render('jovenes/index', { title: 'Jóvenes'});
+});
+
+router.get('/nuevo', function(req, res, next) {
+  res.render('jovenes/nuevo', { title: 'Nuevo Jóven'});
 });
 
 module.exports = router;
