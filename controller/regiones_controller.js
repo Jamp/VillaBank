4cef87e6b1e1700debf1645b2ef35fe419a5370c
Jaro Marval <jampgold@gmail.com>
2015-10-02 16:56:45 -0430

    Regiones vistas de nuevo, editar y borrar

--- controller/regiones_controller.js
+++ controller/regiones_controller.js
@@ -17,3 +17,33 @@ exports.index =  function (req, res) {
         res.render('regiones/index', { title: 'Regiones', regiones: regiones });
     });
 };
+
+
+exports.new = function (req, res) {
+    var region = {
+        id: false,
+        nombre: ''
+    };
+    res.render('regiones/region', { title: 'Nueva Región', region: region });
+};
+
+exports.edit = function (req, res) {
+    var regionId = req.params.regionId;
+
+    models.Regiones.findById(regionId)
+    .then(function(region){
+        res.render('regiones/region', { title: 'Editar Región', region: region });
+    });
+};
+
+exports.delete = function(req, res) {
+    var regionId = req.params.regionId;
+
+    models.Regiones.findById(regionId)
+    .then(function(region){
+        region.destroy()
+        .then(function(){
+            res.redirect('/regiones');
+        });
+    });
+};
--- routes/regiones.js
+++ routes/regiones.js
@@ -4,12 +4,12 @@ var router = express.Router();
 var regionesController = require('../controller/regiones_controller');
 
 router.get('/', regionesController.index);
-/*router.get('/nuevo', regionesController.nuevo);
-router.get('/editar', regionesController.editar);
-router.get('/borrar', regionesController.borrar);
+router.get('/nuevo', regionesController.new);
+router.get('/:regionId(\\d+)/editar', regionesController.edit);
+router.get('/:regionId(\\d+)/borrar', regionesController.delete);
 
-router.post('/create', regionesController.create);
-router.post('/update', regionesController.update);*/
+//router.post('/create', regionesController.create);
+//router.post('/update', regionesController.update);
 
 
 router.get('/:regionId(\\d+)/distritos', regionesController.get);
