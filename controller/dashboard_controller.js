5e1f607f6e81497060f40e3a33a3f0bc0a375464
Jaro Marval <jampgold@gmail.com>
2015-10-05 10:54:50 -0430

    Comenzando a implementar socket.io en el proyecto

---
+++ controller/dashboard_controller.js
@@ -0,0 +1,15 @@
+var socket = require('../bin/www');
+var models = require('../models/models.js');
+
+exports.index = function (req, res, next) {
+    console.log(socket.io);
+    /// Socket ///
+
+    socket.io.on('connection', function (socket) {
+        console.log('Conectado!!!');
+    });
+
+    /// Socket ///
+
+    res.render('index', { title: 'Express' });
+}
